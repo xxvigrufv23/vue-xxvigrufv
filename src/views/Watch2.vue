7ea520813cc9ae6f4ca7486a9bc0da51ba957b27
jaeyoun.choi <jaeyoun.choi@softcamp.co.kr>
2021-10-21 17:56:36 +0900

    20211021 cjy computed와 watch

---
+++ src/views/Watch2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+  <h1> Full Name : {{fullName}}</h1>
+  <button type="button" @click="changeName">이름 변경</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: 'jaeyoun',
+      lastName: 'Choi',
+      fullName: 'jaeyoun choi'
+    };
+  },
+  watch: {
+    firstName() {
+      this.fullName = this.firstName + ' ' + this.lastName;
+    },
+    lastName() {
+      this.fullName = this.firstName + ' ' + this.lastName;
+    }
+  },
+  methods: {
+    changeName() {
+      this.firstName = 'june';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
